2f30e6e0f861c05dcd3b69a1342d3efbf4d171c2
HannahJacqueline <itshannahjacqueline@gmail.com>
2025-01-29 18:04:20 +0800

    feat: create angular app and retrieve data from server to client app

--- API/Program.cs
+++ API/Program.cs
@@ -9,10 +9,16 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors((opt) => opt.AddPolicy(
+    "DatingApp", 
+    (policy) => policy.WithOrigins("http://localhost:4200", "https://localhost:4200").AllowAnyMethod().AllowAnyHeader()
+));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors("DatingApp");
+
 app.MapControllers();
 
 app.Run();
