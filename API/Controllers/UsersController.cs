cf50a3d534346d06601218c139d600545ddf4d00
HannahJacqueline <itshannahjacqueline@gmail.com>
2025-02-01 10:37:10 +0800

    feat[register api]: update database and Users table to prepare for authentication feature

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,26 +4,25 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers
+namespace API.Controllers;
+
+public class UsersController(DataContext context) : BaseAPIController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController(DataContext context) : ControllerBase
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() {
-            var users = await context.Users.ToListAsync();
+        var users = await context.Users.ToListAsync();
 
-            return users;
-        }
+        return users;
+    }
 
-        [HttpGet("{id:int}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id) {
-            var user = await context.Users.FindAsync(id);
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await context.Users.FindAsync(id);
 
-            if (user == null) return NotFound();
+        if (user == null) return NotFound();
 
-            return user;
-        }
+        return user;
     }
 }
